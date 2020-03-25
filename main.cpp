7bc658c42101f0d403c7fac7b8530cf395a0cda7
aswingler1 <aswingler@mail.csuchico.edu>
2020-03-25 11:16:24 -0700

    completed in class assignment 3/25

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@ using std::cout;
 
 int main()
 {
-    TopTenList* languages = new TopTenList("Programming Languages");
+    TopTenList* languages = new TopTenList();
     Hyperlink cpp;
 
     cpp.text = "C++";
     cpp.url = "https://www.cplusplus.com/";
     languages->set_at(1, cpp);
 
-    cout<<"Top Ten "<<languages->get_title()<<"\n";
 
     return 0;
 }
